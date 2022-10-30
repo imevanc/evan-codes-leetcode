4bba6d0e812470c2100f63625a8501e3d035862b
imevanc <evan.charalampidis@gmail.com>
2022-10-30 13:09:06 +0000

    code before the video

---
+++ 392.IsSubsequence/src/isSubsequence.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function (s, t) {
+  let sPointer = 0;
+  let tPointer = 0;
+  while (sPointer <= s.length - 1 && tPointer <= t.length - 1) {
+    if (s[sPointer] === t[tPointer]) {
+      sPointer++;
+    }
+    tPointer++;
+  }
+  return sPointer === s.length;
+};
